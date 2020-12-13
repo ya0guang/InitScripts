405663531c02e8d7dce674ee6f38ce9f924c80da
ya0guang <hbchen001@gmail.com>
2020-12-12 22:08:45 -0500

    Update apt_common.sh

--- apt_common.sh
+++ apt_common.sh
@@ -1,12 +1,14 @@
 # Environment
 # Update System
 sudo apt update && apt upgrade
-sudo apt install vim curl wget zsh git -y
+sudo apt install vim curl wget zsh git fonts-powerline -y
 
 # Oh My Zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# chagne theme to agnoster
-sed -i 's#ZSH_THEME="robbyrussell"#ZSH_THEME="agnoster"#g' ./.zshrc 
+# download powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# chagne theme to powerlevel10k
+sed -i 's#ZSH_THEME="robbyrussell"#ZSH_THEME="powerlevel10k/powerlevel10k"#g' ./.zshrc 
 
 # VScode
 sudo snap install --classic code

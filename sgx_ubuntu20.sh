d8abae7abfa1c3767f1b73938b2ffe3ce1f57f1d
Weijie Liu <lpjlwjlpjlwj@126.com>
2021-03-11 00:32:13 -0500

    Update sgx_ubuntu20.sh

--- sgx_ubuntu20.sh
+++ sgx_ubuntu20.sh
@@ -5,7 +5,7 @@
 sudo apt-get install linux-headers-$(uname -r)
 
 git clone https://github.com/intel/linux-sgx-driver.git
-
+cd linux-sgx-driver
 make
 sudo mkdir -p "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx" 
 sudo cp isgx.ko "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx" 
